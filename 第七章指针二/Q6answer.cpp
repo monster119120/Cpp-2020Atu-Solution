1fb04b111cc581b6425a76754ef1abc66593c6a3
azuna <monster119120@gmail.com>
2020-10-26 16:39:42 +0800

    aa

--- 第七章指针二/Q6answer.cpp
+++ 第七章指针二/Q6answer.cpp
@@ -1,47 +1,40 @@
-#include<iostream>
-#include <stdlib.h>  
-#include <stdio.h>  
+#include <iostream>
 #include <cstring>
 using namespace std;
 
-
-
-char* fun(int x) {
-	if (x > 366 || x < 1) {
-		return NULL;
-	}
-
-	int a[15] = { 0,31,29,31,30,31,30,31,31,30,31,30,31 };
-	char monthname[13][20] = { "\0","Jan","Feb","Mar",
-		"Apr","May","Jun","Jul","Aug","Sep",
-		"Oct","Nov","Dec" };
-	
-	int i;
-	for (i = 1; x > a[i] + 1; i++)
-		x -= a[i];
-	a[13] = i;
-	a[14] = x;
-	
-    sprintf(monthname[0], "%d", x); 
-
-	//itoa(x, monthname[0], 10);
-	strcat(monthname[i], " ");
-	strcat(monthname[i], monthname[0]);
-	
-	char* p = new char[strlen(monthname[i]) + 20];//strlen(str)只包括可见字符的长度， 不包括/0, 所以要strlen(str)+1
-	strcpy(p, monthname[i]);
-	return p;
+char *julian(int year,int day)
+{
+    int i,day_1;
+    bool flag=false;
+    char* str[]={"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+    int month_days[]={31,28,31,30,31,30,31,31,30,31,30,31};
+    if((year%4==0&&year%100!=0)||year%400==0)
+        flag=true;
+    if(day>365+flag)
+        return NULL;
+    char* daystr=new char[7];
+    if(flag)
+        month_days[1]++;
+    for(i=0;i<12 && day>0;i++)
+        day-=month_days[i];
+    i--;
+    strcpy(daystr,str[i]);
+    day+=month_days[i];
+    day_1=day/10;
+    day=day%10;
+    daystr[3]=' ';
+    daystr[4]='0'+day_1;
+    daystr[5]='0'+day;
+    daystr[6]='\0';
+    return daystr;
 }
-
-int main() {
-	int n;
-	cin >> n;
-	char* str;
-	str = fun(n);
-	if (str != NULL)
-		cout << str << endl;
-	else
-		cout << "NULL" << endl;
-
-	return 0;
+int main()
+{
+    int year,day;
+    char* res;
+    cin>>year>>day;
+    res=julian(year,day);
+    cout<<res<<endl;
+    delete []res;
+    return 0;
 }
