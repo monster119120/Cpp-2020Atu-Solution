249933ac3c5ba74883886e33f59cef26abaf9de6
azuna <monster119120@gmail.com>
2020-12-02 21:50:34 +0800

    add

--- 第十章类答案/11.简易字符串类/class.cpp
+++ 第十章类答案/11.简易字符串类/class.cpp
@@ -1,2 +1,73 @@
+//
+// Created by azuna on 2020/12/2.
+//
+#include <cstring>
 #include "class.h"
 
+String::String(char *myStr) {
+    this->myStr = new char[strlen(myStr)];
+    strcpy(this->myStr, myStr);
+    myLen = strlen(myStr);
+}
+
+String::String(String &str) {
+    delete[] myStr;
+    this->myStr = new char[str.myLen];
+    strcpy(this->myStr, str.myStr);
+    myLen = str.myLen;
+}
+
+String::~String() {
+    delete [] myStr;
+}
+
+int String::length() {
+    return myLen;
+}
+
+String String::substr(int start, int n) {
+    char *str = new char[n+1];
+    for (int i = 0; i <  + n ; ++i) {
+        str[i] = this->myStr[i+start];
+    }
+
+    String res(str);
+
+    return res;
+}
+
+int String::find_first_of(char c) {
+    for (int i = 0; i < myLen; ++i) {
+        if (myStr[i]==c)
+            return i;
+    }
+
+    return 0;
+}
+
+void String::add_before(int index, char c) {
+    if (myLen == strlen(myStr)){
+        char* temp = myStr;
+        this->myStr = new char[myLen*3];
+        strcpy(myStr, temp);
+        delete[] temp;
+    }
+
+    for (int i = myLen; i > index ; --i) {
+        myStr[i] = myStr[i-1];
+    }
+    myStr[index] = c;
+    myLen ++;
+    myStr[myLen] = '\0';
+}
+
+void String::delete_at(int index) {
+    for (int i = index; i < myLen; ++i) {
+        myStr[i] = myStr[i+1];
+    }
+
+}
+
+void String::printStr() {
+    std::cout<<myStr<<std::endl;
+}
--- 第十章类答案/11.简易字符串类/main.cpp
+++ 第十章类答案/11.简易字符串类/main.cpp
@@ -1,12 +1,48 @@
 #include <iostream>
 #include "class.h"
 
-
 using namespace std;
 
-int main(){
+int main() {
+    char init_str[80];
+    cin.getline(init_str, 80);
+    char cmd;
+    cin>>cmd;
 
-
+    String str(init_str);
+    while (cmd!='@')
+    {
+        switch (cmd) {
+            case '0':
+//                cout<<str.length()<<endl;
+                break;
+            case '1':
+                int start, n;
+                cin>>start>>n;
+                str.substr(start,n-start+1).printStr();
+                break;
+            case '2':
+                char find_char;
+                cin>>find_char;
+                cout<<str.find_first_of(find_char)<<endl;
+                break;
+            case '3':
+                int index;
+                char c;
+                cin>>index>>c;
+                str.add_before(index+1,c);
+                break;
+            case '4':
+                int ind;
+                cin>>ind;
+                str.delete_at(ind);
+                break;
+            case '5':
+                str.printStr();
+                break;
+        }
+        cin>>cmd;
+    }
 
     return 0;
 }
